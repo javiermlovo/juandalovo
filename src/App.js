46f7c39e014a4eecd10046b0494c2646fb370e8f
Javier Medina <javiermlovo@gmail.com>
2023-04-18 00:37:11 -0500

    add: I install bootstrap and sass to be use in the project, I also add the images of the project and the folder of sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo-juandalovo.webp';
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo mb-3" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          CORPORATIVE - ADVERTISING - INTERIOR DESIGN - PORTRAIT
+        </p>
+        <p>
+          This project is a portfolio of a professional photographer and publisher
         </p>
         <a
           className="App-link"
@@ -15,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          More info click here
         </a>
       </header>
     </div>
